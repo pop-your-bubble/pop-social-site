4574c02e0c9f859590fd3285b1c2f1136aea8821
Doris Hui <dorishui225@gmail.com>
2019-10-01 03:03:52 -0700

    made navbar right align, edited footer

--- client/NavBar.js
+++ client/NavBar.js
@@ -4,6 +4,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import Box from "@material-ui/core/Box";
 
 const NavBar = () => {
   const [value, setValue] = useState(0);
@@ -18,20 +19,24 @@ const NavBar = () => {
     <div>
       <AppBar color="inherit" position="static">
         <Toolbar>
-          <img
-            src="https://popsocial.app/img/pop-500px.png"
-            style={{ width: 100 }}
-          />
-          <Tabs
-            onChange={handleChange}
-            value={value}
-            indicatorColor="primary"
-            textColor="primary"
-          >
-            <Tab label="SignUp" value="/" />
-            <Tab label="About Us" value="/about" />
-            <Tab label="Staff" value="/staff" />
-          </Tabs>
+          <Box flexGrow={1}>
+            <img
+              src="https://popsocial.app/img/pop-500px.png"
+              style={{ width: 100 }}
+            />
+          </Box>
+          <Box>
+            <Tabs
+              onChange={handleChange}
+              value={value}
+              indicatorColor="primary"
+              textColor="primary"
+            >
+              <Tab label="SignUp" value="/" />
+              <Tab label="About Us" value="/about" />
+              <Tab label="Staff" value="/staff" />
+            </Tabs>
+          </Box>
         </Toolbar>
       </AppBar>
       {redirected}
