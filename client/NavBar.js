7ea1aae9d862025f73259afcf89c9783bfa40e2d
Joseph Oh <ohyjoseph@gmail.com>
2019-10-01 01:39:12 -0700

    Routing with React Router Redirect

--- client/NavBar.js
+++ client/NavBar.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 
 const NavBar = () => {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
+  const [redirected, setRedirected] = useState(null);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    if (newValue === 0) {
+      setRedirected(<Redirect to="/" />);
+    } else if (newValue === 1) {
+      setRedirected(<Redirect to="/about" />);
+    } else if (newValue === 2) {
+      setRedirected(<Redirect to="/staff" />);
+    }
   };
 
   return (
