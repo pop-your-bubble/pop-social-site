077b12d0299ff3677df515b015b34681f60037fb
Doris Hui <dorishui225@gmail.com>
2019-10-01 01:17:02 -0700

    edited staff page to have descriptions

--- client/Staff.js
+++ client/Staff.js
@@ -1,81 +1,83 @@
 import React from "react";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Grid from '@material-ui/core/Grid';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Grid from "@material-ui/core/Grid";
 
 const staffLeft = [
   {
     name: "Bumblebee Boy",
-    description: "PK THUNDER!!!",
-    imageURL:
-      "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
+    description: "Ness (ネス, Ness) is the silent main protagonist of EarthBound and is analogous to Ninten and Lucas in their respective games. He greatly enjoys baseball; not only are most of his weapons various types of baseball bats, but he can also equip several baseball caps. In the EarthBound Player's Guide, he is said to wear a special baseball cap with his favorite team's logo on the front.",
+    imageURL: "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
   },
   {
     name: "Bumblebee Boy",
-    description: "PK THUNDER!!!",
-    imageURL:
-      "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
+    description: "Ness (ネス, Ness) is the silent main protagonist of EarthBound and is analogous to Ninten and Lucas in their respective games. He greatly enjoys baseball; not only are most of his weapons various types of baseball bats, but he can also equip several baseball caps. In the EarthBound Player's Guide, he is said to wear a special baseball cap with his favorite team's logo on the front.",
+    imageURL: "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
   },
   {
     name: "Bumblebee Boy",
-    description: "PK THUNDER!!!",
-    imageURL:
-      "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
+    description: "Ness (ネス, Ness) is the silent main protagonist of EarthBound and is analogous to Ninten and Lucas in their respective games. He greatly enjoys baseball; not only are most of his weapons various types of baseball bats, but he can also equip several baseball caps. In the EarthBound Player's Guide, he is said to wear a special baseball cap with his favorite team's logo on the front.",
+    imageURL: "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
   },
   {
     name: "Bumblebee Boy",
-    description: "PK THUNDER!!!",
-    imageURL:
-      "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
+    description: "Ness (ネス, Ness) is the silent main protagonist of EarthBound and is analogous to Ninten and Lucas in their respective games. He greatly enjoys baseball; not only are most of his weapons various types of baseball bats, but he can also equip several baseball caps. In the EarthBound Player's Guide, he is said to wear a special baseball cap with his favorite team's logo on the front.",
+    imageURL: "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
+  },
+  {
+    name: "Bumblebee Boy",
+    description: "Ness (ネス, Ness) is the silent main protagonist of EarthBound and is analogous to Ninten and Lucas in their respective games. He greatly enjoys baseball; not only are most of his weapons various types of baseball bats, but he can also equip several baseball caps. In the EarthBound Player's Guide, he is said to wear a special baseball cap with his favorite team's logo on the front.",
+    imageURL: "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
   }
 ];
 
 const staffRight = [
   {
     name: "Bumblebee Boy",
-    description: "PK THUNDER!!!",
-    imageURL:
-      "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
+    description: "Ness (ネス, Ness) is the silent main protagonist of EarthBound and is analogous to Ninten and Lucas in their respective games. He greatly enjoys baseball; not only are most of his weapons various types of baseball bats, but he can also equip several baseball caps. In the EarthBound Player's Guide, he is said to wear a special baseball cap with his favorite team's logo on the front.",
+    imageURL: "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
   },
   {
     name: "Bumblebee Boy",
-    description: "PK THUNDER!!!",
-    imageURL:
-      "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
+    description: "Ness (ネス, Ness) is the silent main protagonist of EarthBound and is analogous to Ninten and Lucas in their respective games. He greatly enjoys baseball; not only are most of his weapons various types of baseball bats, but he can also equip several baseball caps. In the EarthBound Player's Guide, he is said to wear a special baseball cap with his favorite team's logo on the front.",
+    imageURL: "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
   },
   {
     name: "Bumblebee Boy",
-    description: "PK THUNDER!!!",
-    imageURL:
-      "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
+    description: "Ness (ネス, Ness) is the silent main protagonist of EarthBound and is analogous to Ninten and Lucas in their respective games. He greatly enjoys baseball; not only are most of his weapons various types of baseball bats, but he can also equip several baseball caps. In the EarthBound Player's Guide, he is said to wear a special baseball cap with his favorite team's logo on the front.",
+    imageURL: "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
   },
   {
     name: "Bumblebee Boy",
-    description: "PK THUNDER!!!",
-    imageURL:
-      "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
+    description: "Ness (ネス, Ness) is the silent main protagonist of EarthBound and is analogous to Ninten and Lucas in their respective games. He greatly enjoys baseball; not only are most of his weapons various types of baseball bats, but he can also equip several baseball caps. In the EarthBound Player's Guide, he is said to wear a special baseball cap with his favorite team's logo on the front.",
+    imageURL: "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
+  },
+  {
+    name: "Bumblebee Boy",
+    description: "Ness (ネス, Ness) is the silent main protagonist of EarthBound and is analogous to Ninten and Lucas in their respective games. He greatly enjoys baseball; not only are most of his weapons various types of baseball bats, but he can also equip several baseball caps. In the EarthBound Player's Guide, he is said to wear a special baseball cap with his favorite team's logo on the front.",
+    imageURL: "https://pbs.twimg.com/media/CaPjgvPWQAAgQme.jpg"
   }
 ];
 
 const Staff = () => {
   return (
-    <>
+    <div style={{ padding: 20 }}>
       <Grid container spacing={1} justify="center" alignItems="center">
         <Grid item xs={6}>
           <List>
             {staffLeft.map(staff => (
               <ListItem
                 key={staff.name}
-                onClick={() => window.open(staff.link, "_blank")}
               >
                 <img
-                  style={{ width: 60, borderRadius: "50%" }}
+                  style={{ width: 80, borderRadius: "50%" }}
                   src={staff.imageURL}
                 ></img>{" "}
                 <ListItemText
                   style={{ padding: "15px" }}
                   primary={staff.name}
+                  secondary={staff.description}
                 />
               </ListItem>
             ))}
@@ -86,22 +88,22 @@ const Staff = () => {
             {staffRight.map(staff => (
               <ListItem
                 key={staff.name}
-                onClick={() => window.open(staff.link, "_blank")}
               >
                 <img
-                  style={{ width: 60, "border-radius": "50%" }}
+                  style={{ width: 80, "border-radius": "50%" }}
                   src={staff.imageURL}
                 ></img>{" "}
                 <ListItemText
                   style={{ padding: "15px" }}
                   primary={staff.name}
+                  secondary={staff.description}
                 />
               </ListItem>
             ))}
           </List>
         </Grid>
       </Grid>
-    </>
+    </div>
   );
 };
 
