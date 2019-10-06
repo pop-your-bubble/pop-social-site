fc0465f2ebf7a462aab561e6eb70781566848846
Joseph Oh <ohyjoseph@gmail.com>
2019-10-06 04:17:00 -0700

    Changed webpack config to only use file-loader for images

--- webpack.config.js
+++ webpack.config.js
@@ -16,13 +16,6 @@ module.exports = {
       },
       {
         test: /\.(jpg|png|svg)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 25000
-        }
-      },
-      {
-        test: /\.(jpg|png|svg)$/,
         loader: 'file-loader',
         options: {
           name: '[path][name].[hash].[ext]'
