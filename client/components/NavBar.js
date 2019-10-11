5d43bbb3e2bdb9951fa6ebe77c3a1c3ff4214c76
Joseph Oh <ohyjoseph@gmail.com>
2019-10-11 10:59:02 -0700

    NavBar hides and reappears on scroll

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -6,11 +6,15 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Box from "@material-ui/core/Box";
+import Slide from "@material-ui/core/Slide";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 
 import imgPopLogo from "../images/pop-logo.svg";
 
 const NavBar = ({ history }) => {
   const [path, setPath] = useState(history.location.pathname);
+
+  const trigger = useScrollTrigger();
 
   const handleChange = (event, newValue) => {
     // Adds to history whenever tab is clicked and is navigating to different page
@@ -27,31 +31,30 @@ const NavBar = ({ history }) => {
   });
 
   return (
-    <div position="sticky">
-      <AppBar color="inherit">
-        <Toolbar>
-          <Box flexGrow={1}>
-            <Link to="/" >
-              <img
-                src={imgPopLogo}
-                style={{ width: 100 }}
-              />
-            </Link>
-          </Box>
-          <Box>
-            <Tabs
-              onChange={handleChange}
-              value={path}
-              indicatorColor="primary"
-              textColor="primary"
-            >
-              <Tab label="SignUp" value="/" />
-              <Tab label="About" value="/about" />
-              <Tab label="Team" value="/team" />
-            </Tabs>
-          </Box>
-        </Toolbar>
-      </AppBar>
+    <div>
+      <Slide appear={false} direction="down" in={!trigger}>
+        <AppBar color="inherit" position="fixed">
+          <Toolbar>
+            <Box flexGrow={1}>
+              <Link to="/">
+                <img src={imgPopLogo} style={{ width: 100 }} />
+              </Link>
+            </Box>
+            <Box>
+              <Tabs
+                onChange={handleChange}
+                value={path}
+                indicatorColor="primary"
+                textColor="primary"
+              >
+                <Tab label="SignUp" value="/" />
+                <Tab label="About" value="/about" />
+                <Tab label="Team" value="/team" />
+              </Tabs>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Slide>
       <Redirect to={path} /> {/* Changes page based on path */}
     </div>
   );
